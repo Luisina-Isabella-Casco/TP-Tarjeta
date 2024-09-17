8c5019c79e1628175b8dd13003025734b85eaa8c
Luisina-Isabella-Casco <110426198+Luisina-Isabella-Casco@users.noreply.github.com>
2024-09-17 16:53:35 -0300

    Add files via upload
    Iteración 1 del Trabajo Práctico Tarjeta

---
+++ TP Tarjeta/Class0.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Tarjeta
+{
+    public class Tarjeta
+    {
+        public int Saldo { get; private set; }
+        private const int LimiteSaldo = 9900;
+
+        public Tarjeta(int Saldoinicial)
+        {
+            Saldo = Saldoinicial;
+        }
+
+        public bool CargaSaldo(int monto)
+        {
+            if (monto % 1000 == 0 && monto >= 2000 && monto <= 9000 && Saldo + monto <= LimiteSaldo)
+
+            {
+                Saldo += monto;
+                Console.WriteLine("Carga realizada con éxito");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("No se pudo realizar la carga");
+                return false;
+            }
+        }
+
+        public bool DescontarSaldo(int tarifa)
+        {
+            if (Saldo >= tarifa)
+            {
+                    Saldo -= tarifa;
+
+                    return true;
+            }
+            else
+            {
+                    Console.WriteLine("Saldo insuficiente");
+                    return false;
+            }
+
+
+        }
+
+
+    }
+ } 
+
---
+++ TP Tarjeta/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Tarjeta
+{
+    public class Colectivo 
+    {
+        private const int TarifaBasica = 940;
+
+        public Boleto PagarCon (Tarjeta tarjeta)
+        {
+            if (tarjeta.DescontarSaldo(TarifaBasica))
+            {
+           
+                return new Boleto(TarifaBasica);
+            }
+            return null;
+        }
+    }
+}
