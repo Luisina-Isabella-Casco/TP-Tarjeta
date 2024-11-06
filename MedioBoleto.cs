0dede851b4450809f143a141b31a32f62bfce319
Luisina-Isabella-Casco <110426198+Luisina-Isabella-Casco@users.noreply.github.com>
2024-11-05 23:22:16 -0300

    Add files via upload

--- MedioBoleto.cs
+++ MedioBoleto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP_Tarjeta
+/*namespace TP_Tarjeta
 {
     public class MedioBoleto : Tarjeta
     {
@@ -18,23 +18,71 @@ namespace TP_Tarjeta
         }
 
         public MedioBoleto(int saldoInicial) : base(saldoInicial) { }
-
-        /*public int DescontarMedioBoleto()
-        {
-            int tarifaMedioBoleto = TarifaBasica / 2;
-
-            if (saldoPublico >= tarifaMedioBoleto)
-            {
-                saldoPublico -= tarifaMedioBoleto;
-                return saldoPublico;
-            }
-
-            else
-            {
-                Console.WriteLine("Saldo insuficiente");
-                return saldoPublico;
-            }*/
         }
 
     }
 
+  public int DescontarMedioBoleto()
+      {
+          int tarifaMedioBoleto = TarifaBasica / 2;
+
+          if (saldoPublico >= tarifaMedioBoleto)
+          {
+              saldoPublico -= tarifaMedioBoleto;
+              return saldoPublico;
+          }
+
+          else
+          {
+              Console.WriteLine("Saldo insuficiente");
+              return saldoPublico;
+          }*/
+
+namespace TP_Tarjeta
+{
+    public class MedioBoleto : Tarjeta
+    {
+       protected const int TarifaBasica = 940;
+       
+        private const decimal LimiteNegativo = -480;
+
+        private List<DateTime> viajesRealizados; //lista de fecha y hora de viajes realizados
+
+        public MedioBoleto(int saldoInicial) : base(saldoInicial)
+        {
+            viajesRealizados = new List<DateTime>();
+        }
+
+        public bool PagarConMedioBoleto()
+        {
+            DateTime ahora = DateTime.Now;
+
+            if (viajesRealizados.Count < 4) {
+
+                if ((ahora - viajesRealizados.Last()).TotalMinutes < 5)
+                {
+                    Console.WriteLine("Debe esperar 5 minutos entre viajes.");
+                    return false;
+                }
+                else
+                {
+                    viajesRealizados.Add(ahora);
+                    Colectivo.DescontarSaldo(this); // 'this': instancia actual de la tarjeta
+                    Colectivo.PagarCon(this);
+                    return true;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Ya se han realizado 4 viajes en el día.");
+                if(saldoPublico - TarifaBasica < LimiteNegativo)
+                {
+                    saldoPublico -= TarifaBasica;
+                    Colectivo.PagarCon(this);
+                }
+                return true;
+            }
+        }
+    }
+}
